97f4089d35f334c07e8f4835350f8c87c1f4ea41
Oskar Svanström <65446534+Osksacho@users.noreply.github.com>
2023-09-11 21:58:03 +0200

    TA-20 Basic test environment (#10)
    * basic movement test
    
    * Creates a basic test environment
    
    * Remove camera
    
    * Update .gitignore
    
    * Update TestEnvironment.tscn
    
    ---------
    
    Co-authored-by: Anton Forsman <10177340+anforsm@users.noreply.github.com>
    Co-authored-by: Oskar Svanström <oskarsva@kth.se>

--- Scripts/Cube.cs
+++ Scripts/Cube.cs
@@ -6,11 +6,32 @@ public partial class Cube : MeshInstance3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Cube Ready");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Rotate(Vector3.Up, 2*(float)delta);
+		float moveSpeed = 5f;
+		//Rotate(Vector3.Up, 2*(float)delta);
+		if (Input.IsActionPressed("move_right"))
+		{
+			Translate(Vector3.Right * (float)delta * moveSpeed);
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			Translate(Vector3.Left * (float)delta * moveSpeed);
+		}
+
+		if (Input.IsActionPressed("move_forward"))
+		{
+			Translate(Vector3.Forward * (float)delta * moveSpeed);
+		}
+
+		if (Input.IsActionPressed("move_backward"))
+		{
+			Translate(Vector3.Back * (float)delta * moveSpeed);
+		}
 	}
 }
