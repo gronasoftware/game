29102675dc61ffbdf458a7d96db9e2cc4633162d
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-09-11 00:42:04 -0500

    wonky cursor aware rotation

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,28 +7,33 @@ public partial class Player : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
+		// https://ask.godotengine.org/25922/how-to-get-3d-position-of-the-mouse-cursor
 
-		if (Input.IsActionPressed("ui_right"))
-		{
-			Rotate(Vector3.Up, 2 * (float)delta);
-		}
+		var camera = GetViewport().GetCamera3D();
+		var mousePosition2D = GetViewport().GetMousePosition();
+		var dropPlane = Plane.PlaneXZ;
+		var _mousePosition3D = dropPlane.IntersectsRay(
+			camera.ProjectRayOrigin(mousePosition2D),
+			camera.ProjectRayNormal(mousePosition2D)
+		);
 
-		// Get the input direction and handle the movement/deceleration.
+		Vector3 mousePosition3D = _mousePosition3D ?? Vector3.Zero;
+		GD.Print(Transform.Origin);
+
+		// Rotate to face the mouse.
+		Vector3 lookAt = mousePosition3D - Transform.Origin;
+		lookAt.Y = 0;
+		//GD.Print(Transform);
+		Transform = Transform.LookingAt(lookAt, Vector3.Up);
+		//GD.Print(Transform);
+
+		//if (Input.IsActionPressed("ui_right"))
+		//{
+		//	Rotate(Vector3.Up, 2 * (float)delta);
+		//}
+
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
+		Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+		Position += direction * (float)delta * Speed;
 	}
 }
