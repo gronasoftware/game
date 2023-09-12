eebb4fe032ef4732aa9fe36412aaadc3d1e6298d
Oskar Svanström <oskar.svanstrom@hotmail.com>
2023-09-12 20:08:03 +0200

    TA-23 Trailing camera

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+
+
+public partial class CameraController : Camera3D
+{
+    [Export]
+    public Node3D target;
+
+    [Export]
+    public Vector3 offset = new Vector3(0, 5, 5);
+
+    [Export]
+    public float TrailingSpeed = 5.0f;
+
+    private Vector3 pivot = new Vector3();
+
+    public override void _Process(double delta)
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.GlobalPosition;
+
+            pivot = pivot.Lerp(desiredPosition, (float)delta * TrailingSpeed);
+
+            // Update cameras global position
+            GlobalPosition = pivot + offset;
+
+            LookAt(pivot, Vector3.Up);
+        }
+    }
+}
+    
