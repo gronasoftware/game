512f205700892486821c36e8db2a6478d31d8988
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-09-14 22:32:35 -0500

    basic healthbar

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public partial class Enemy : CharacterBase
 
 	public override void _PhysicsProcess(double delta)
 	{
+		var healthbar = (ProgressBar)GetNode("SubViewport/HealthBar");
+		healthbar.Value = health;
 		MoveAndSlide();
 	}
 
