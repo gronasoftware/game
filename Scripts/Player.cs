ce52ef90984ca00d5c4e7ecac9f4741d354133b2
Carl Haneklint <carlhaneklint@gmail.com>
2023-09-17 22:06:18 +0200

    added player gravity and formatted code

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,68 +2,155 @@ using Godot;
 using System;
 using System.Diagnostics;
 
-public partial class Player : CharacterBase 
+public partial class Player : CharacterBase
 {
-	[Export]
-	public const float Speed = 3.5f;
-	private MeshInstance3D mousePositionSphere = null;
+    private MeshInstance3D mousePositionSphere = null;
 
-	[Export]
-	private AnimationPlayer animPlayer;
+    [Export]
+    private AnimationPlayer animPlayer;
 
-		public override void _PhysicsProcess(double delta)
-	{
-		// https://ask.godotengine.org/25922/how-to-get-3d-position-of-the-mouse-cursor
+    public override void _PhysicsProcess(double delta)
+    {
+        ProcessInput((float)delta);
+        CalculateVelocity((float)delta);
+        CalculateDirection();
+        // https://ask.godotengine.org/25922/how-to-get-3d-position-of-the-mouse-cursor
 
-		var camera = GetViewport().GetCamera3D();
-		var mousePosition2D = GetViewport().GetMousePosition();
-		// After we turn on gravity, we should update this to use the plane that the player is standing on (instead of the Y=0 plane)
-		var dropPlane = new Plane(Vector3.Up, 0);
-		//var dropPlane = new Plane(Vector3.Up, Transform.Origin.Y);
+        var camera = GetViewport().GetCamera3D();
+        var mousePosition2D = GetViewport().GetMousePosition();
+        // After we turn on gravity, we should update this to use the plane that the player is standing on (instead of the Y=0 plane)
+        //var dropPlane = new Plane(Vector3.Up, 0);
+        var dropPlane = new Plane(Vector3.Up, Transform.Origin.Y);
 
-		Vector3 mousePosition3D = dropPlane.IntersectsRay(
-			camera.ProjectRayOrigin(mousePosition2D),
-			camera.ProjectRayNormal(mousePosition2D)
-		) ?? Vector3.Forward;
+        Vector3 mousePosition3D =
+            dropPlane.IntersectsRay(
+                camera.ProjectRayOrigin(mousePosition2D),
+                camera.ProjectRayNormal(mousePosition2D)
+            ) ?? Vector3.Forward;
 
-		// DEBUG STUFF:::
-		// Draw a sphere at mousePosition3D
-		var sphere = new SphereMesh();
-		sphere.Radius = 0.1f;
-		sphere.Height = 0.1f;
-		var sphereInstance = new MeshInstance3D();
-		sphereInstance.Mesh = sphere;
-		// Again, this should be updated after we turn on gravity
-		sphereInstance.Position = new Vector3(mousePosition3D.X, 0, mousePosition3D.Z);
-		mousePositionSphere?.QueueFree();
-		mousePositionSphere = sphereInstance;
-		GetParent().AddChild(sphereInstance);
+        // DEBUG STUFF:::
+        // Draw a sphere at mousePosition3D
+        var sphere = new SphereMesh();
+        sphere.Radius = 0.1f;
+        sphere.Height = 0.1f;
+        var sphereInstance = new MeshInstance3D();
+        sphereInstance.Mesh = sphere;
+        // Again, this should be updated after we turn on gravity
+        sphereInstance.Position = new Vector3(
+            mousePosition3D.X,
+            mousePosition3D.Y,
+            mousePosition3D.Z
+        );
+        mousePositionSphere?.QueueFree();
+        mousePositionSphere = sphereInstance;
+        GetParent().AddChild(sphereInstance);
 
-		Transform = Transform.LookingAt(new Vector3(mousePosition3D.X, Transform.Origin.Y, mousePosition3D.Z), Vector3.Up);
+        Transform = Transform.LookingAt(
+            new Vector3(mousePosition3D.X, Transform.Origin.Y, mousePosition3D.Z),
+            Vector3.Up
+        );
 
-		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-		Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+        if (Input.IsActionJustPressed("shoot"))
+        {
+            EmitSignal(SignalName.ShotFired, Transform.Origin, -Transform.Basis.Z);
+        }
 
-		Velocity = direction * Speed;
+        if (animPlayer != null)
+        {
+            if (Velocity.Length() != 0 && animPlayer.CurrentAnimation != "run")
+                animPlayer.Play("run");
+            if (Velocity.Length() == 0 && animPlayer.CurrentAnimation != "Idle")
+                animPlayer.Play("Idle");
+        }
 
-		if (Input.IsActionJustPressed("shoot"))
-		{
-			EmitSignal(SignalName.ShotFired, Transform.Origin, -Transform.Basis.Z);
-		}
+        MoveAndSlide();
+    }
 
-		if(animPlayer != null)
-		{
-			if (Velocity.Length() != 0 && animPlayer.CurrentAnimation != "run")
-				animPlayer.Play("run");
-			if (Velocity.Length() == 0 && animPlayer.CurrentAnimation != "Idle")
-				animPlayer.Play("Idle");
-		}
+    public override void NoMoreHealth()
+    {
+        GD.Print("Game Over!");
+    }
 
-		MoveAndSlide();
-	}
+    #region Input
 
-		public override void NoMoreHealth()
-		{
-			GD.Print("Game Over!");
-		}
+    private Vector2 inputDir;
+
+    private void ProcessInput(float delta)
+    {
+        inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+    }
+
+    #endregion
+
+    #region Velocity
+    private Vector3 velocity;
+    private float previousVelocityY,
+        newVelocityY;
+    private float previuosVelocityX,
+        newVelocityX;
+    private float previuosVelocityZ,
+        newVelocityZ;
+
+    [Export]
+    public const float speed = 5f;
+
+    /// <summary>
+    /// Calculate the bi-velocity and apply it to the character body
+    /// </summary>
+    /// <param name="delta"></param>
+    private void CalculateVelocity(float delta)
+    {
+        velocity = Velocity;
+        CalculateVelocityY(ref velocity, delta);
+        CalculateVelocityXZ(ref velocity, delta);
+        Velocity = velocity;
+    }
+
+    private void CalculateVelocityY(ref Vector3 vel, float delta)
+    {
+        ApplyGravity(ref vel, delta);
+    }
+
+    private void CalculateVelocityXZ(ref Vector3 vel, float delta)
+    {
+        previuosVelocityX = velocity.X;
+        previuosVelocityZ = velocity.Z;
+        newVelocityX = direction.X * speed;
+        newVelocityZ = direction.Z * speed;
+        vel.X = (previuosVelocityX + newVelocityX) * .5f;
+        vel.Z = (previuosVelocityZ + newVelocityZ) * .5f;
+    }
+    #endregion
+
+    #region Gravity
+    [Export]
+    private const float gravity = 9.8f;
+
+    [Export]
+    private const float terminalVelocity = 600f;
+
+    /// <summary>
+    /// Apply gravity to the velocity
+    /// </summary>
+    /// <param name="vel"></param>
+    /// <param name="delta"></param>
+    private void ApplyGravity(ref Vector3 vel, float delta)
+    {
+        if (IsOnFloor())
+            return;
+        previousVelocityY = velocity.Y;
+        newVelocityY = vel.Y - gravity * delta;
+        newVelocityY = Mathf.Clamp(newVelocityY, -Mathf.Inf, terminalVelocity);
+        vel.Y = (previousVelocityY + newVelocityY) * .5f;
+    }
+    #endregion
+
+    #region Direction
+    private Vector3 direction;
+
+    private void CalculateDirection()
+    {
+        direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+    }
+    #endregion
 }
